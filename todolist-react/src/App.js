73d0dfa20421bd3e243f9d84981328a303ec8bd6
hyoseongLee <hyoseong123@naver.com>
2024-10-05 00:21:44 +0900

    20241005_todo

--- todolist-react/src/App.js
+++ todolist-react/src/App.js
@@ -5,6 +5,7 @@ import Todolist from './component/Todolist';
 import { MdAddCircle } from 'react-icons/md';
 import Todoinser from './component/Todoinser';
 
+let nextId = 4;
 
 const App = () => {
   const [inserToggle,setinserToggle] = useState(false);
@@ -28,15 +29,47 @@ const App = () => {
   ])
 
   const oninserToggle = () => {
-    setinserToggle(prev => !prev)
+    setinserToggle(prev => !prev);
   };
+
+  const oninserTodo = text => {
+    if (text === "") {
+      return alert("할 일을 입력해주세요.");
+    } else {
+      const todo = {
+        id: nextId,
+        text,
+        checked: false
+      };
+      setTodos(todos => todos.concat(todo));
+      nextId++;
+    }
+  };
+ 
+  const onCheckToggle = id => {
+    setTodos(todos => 
+      todos.map(todo => 
+        todo.id === id ? {...todo, checked: !todo.checked} : todo
+    )
+   );
+  };
+
+
   return (
     <Todo todoLength = {todos.length}>
-      <Todolist todos={todos} />
-      <div className="add-todo-button" onCilck={oninserToggle}>
+      <Todolist 
+      todos={todos} 
+      onCheckToggle={onCheckToggle}
+      />
+      <div className="add-todo-button" onClick={oninserToggle}>
         <MdAddCircle />
       </div>
-      {inserToggle && <Todoinser oninserToggle={oninserToggle} />}
+      {inserToggle && (
+      <Todoinser 
+      oninserToggle={oninserToggle} 
+      oninserTodo={oninserTodo} 
+      />
+    )}
     </Todo>
   );
 };
--- todolist-react/src/component/Todoinser.js
+++ todolist-react/src/component/Todoinser.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdAddCircle } from 'react-icons/md';
+import './Todoinser.css';
 
-const Todoinser = ({oninserToggle}) => {
-    return 
+const Todoinser = ({oninserToggle, oninserTodo }) => {
+    const [value, setValue] = useState("");
+
+    const onChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    const onSubmit =(e) => {
+        e.preventDefault();
+        oninserTodo(value);
+        setValue("");
+        oninserToggle();
+    };
+
+
+    return (
     <div>
         <div className="background" onClick={oninserToggle}> </div>
-        <form>
-            <input></input>
-            <button type="submit"><MdAddCircle />
+        <form onSubmit={onSubmit}>
+            <input placeholder="please type" 
+            value={value} 
+            onChange={onChange}
+            ></input>
+            <button type="submit">
+                <MdAddCircle />
             </button>
         </form>
     </div>
-}
+    );
+};
 
 export default Todoinser;
